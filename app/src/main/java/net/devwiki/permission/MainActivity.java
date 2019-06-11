a6021beefb7b7b83807549e49b2f3cc186c732f1
zhangyazhou <zhangyazhou@xylink.com>
2019-06-11 20:30:19 +0800

    去除不必要引用

--- app/src/main/java/net/devwiki/permission/MainActivity.java
+++ app/src/main/java/net/devwiki/permission/MainActivity.java
@@ -1,14 +1,9 @@
 package net.devwiki.permission;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import net.devwiki.log.DevLog;
 import net.devwiki.permission.jumpactivirty.JumpPermissionManager;
 
 /**
